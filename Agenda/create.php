cae4bb7052e69ef61b217860addff1e5834a5ffd
Andrey Marques <andreymarques822@gmail.com>
2023-03-03 19:49:36 -0300

    Fullstack: criação da pagina de criação de contatos

---
+++ Agenda/create.php
@@ -0,0 +1,12 @@
+<?php
+    include_once("config/url.php");
+    include_once("templates/header.php");
+    
+?>
+
+    <h1>Criando Contato</h1>
+    
+
+<?php
+    include_once("templates/footer.php");
+?>

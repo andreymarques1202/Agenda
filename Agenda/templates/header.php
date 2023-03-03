e6259c457618acc6a1852f192fe70f9b583b3832
Andrey Marques <andreymarques822@gmail.com>
2023-03-03 19:51:04 -0300

    Fullstack: criação do cabeçalho e configurando a session

---
+++ Agenda/templates/header.php
@@ -0,0 +1,41 @@
+<?php
+
+use PSpell\Config;
+
+    include_once("config/url.php");
+    include_once("config/connection.php");
+
+    //limpa a mensagem
+
+    if(isset($_SESSION['msg'])) {
+        $printMsg = $_SESSION['msg'];
+        $_SESSION['msg'] = '';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONT-AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+    <title>Agenda de Contatos</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<? $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div class="navbar-nav">
+                <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Agenda</a>
+                <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>create.php">Adicionar Contato</a>
+            </div>
+        </nav>
+    </header>

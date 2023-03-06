e5dc5b886a66266366b49cdbdd3a1ea82ad377a8
Andrey Marques <andreymarques822@gmail.com>
2023-03-06 19:30:23 -0300

    Frontend: Finalização do index

--- Agenda/index.php
+++ Agenda/index.php
@@ -22,11 +22,11 @@
                 <tbody>
                     <?php foreach($contacts as $contact): ?>
                         <tr>
-                            <td scope="row"><?= $contact['id'] ?></td>
+                            <td scope="row" class="col-id"><?= $contact['id'] ?></td>
                             <td scope="row"><?= $contact['name'] ?></td>
                             <td scope="row"><?= $contact['phone'] ?></td>
                             <td class="actions">
-                                <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                                <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
                                 <a href="#"><i class="fas fa-edit edit-icon"></i></a>
                                 <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
                             </td>

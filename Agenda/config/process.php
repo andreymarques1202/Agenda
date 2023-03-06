dc57780c68d7e449abfe8175821b3e1b1c5763c6
Andrey Marques <andreymarques822@gmail.com>
2023-03-06 19:28:47 -0300

    Backend: Aplicação da logica

--- Agenda/config/process.php
+++ Agenda/config/process.php
@@ -4,14 +4,48 @@
     include_once("connection.php");
     include_once("url.php");
     
+    $data = $_POST;
 
-    $contacts = [];
+    //MODIFICAÇÕES NO BANCO
+    if(!empty($data)) {
+        print_r($data); exit;
 
-    $query = "SELECT * FROM contacts";
+        //SELEÇÃO DE DADOS
+    } else {
+        //retorna o dado de um contato
+    $id;
 
-    $stmt = $connect->prepare($query);
+    if(!empty($_GET['id'])) {
+        $id = $_GET['id'];
+    }
 
-    $stmt->execute();
+    if(!empty($id)) {
+        $query = "SELECT * FROM contacts WHERE id = :id";
 
-    $contacts = $stmt->fetchAll();
+        $stmt = $connect->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+    } else {
+        //Retorna todos os contatos
+        $contacts = [];
+
+        $query = "SELECT * FROM contacts";
+
+        $stmt = $connect->prepare($query);
+
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+    }
+    }
+
+    
+
+    
+    
+    
 ?>
